b0a690534bf44504d13799a454bedbbe58351ce3
Robert Scott Reis <reis.robert.s@gmail.com>
2018-03-16 11:16:33 -0700

    serve bundle correctly

--- frontend/lib/server.js
+++ frontend/lib/server.js
@@ -4,5 +4,6 @@ const path = require('path');
 const app = express();
 const root = {root: path.resolve(__dirname, '..')};
 
+app.use('/bundle.js', (req, res) => res.sendFile('public/bundle.js', root));
 app.use('*', (req, res) => res.sendFile('public/index.html', root));
 app.listen(8080, () => console.log('Listening to 8080'));

be43b0ef241490be3e87cb97ea22170e40043cda
Robert Scott Reis <reis.robert.s@gmail.com>
2018-03-16 11:16:41 -0700

    include react explicitly

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,7 +1,7 @@
-import { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return <h1>Hello world!</h1>;
   }

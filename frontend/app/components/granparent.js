98566e140d88aad85ea10a1e81448d869616cbe7
Trish Griego <trish.griego@moz.com>
2018-04-04 10:58:22 -0700

    Add grandparent class

---
+++ frontend/app/components/granparent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Grandparent extends React.component {
+  render() {
+    return <div classname="grandparent ten columns">
+      <div>Alan and Betty</div>
+      <div>Ethan and Fran</div>
+      <div>Harry and Irene</div>
+    </div>
+
+  }
+}
